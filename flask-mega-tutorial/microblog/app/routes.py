2ea98ee9e0cc63fdff959521512599ef9a26e963
cadbane <33034420+cadbane@users.noreply.github.com>
2017-12-12 21:43:32 +0100

    [c] flask-mega-tutorial/microblog/app/routes.py [c] flask-mega-tutorial/microblog/app/templates/index.html add conditional statement

--- flask-mega-tutorial/microblog/app/routes.py
+++ flask-mega-tutorial/microblog/app/routes.py
@@ -5,4 +5,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'name': 'cdbn'}
-    return render_template('index.html', user=user)
+    return render_template('index.html', title='title', user=user)
