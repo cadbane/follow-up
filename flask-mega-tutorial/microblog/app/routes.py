15e0514fb05223a3c27c6626d2e28ef6c7015009
cadbane <33034420+cadbane@users.noreply.github.com>
2017-12-12 21:54:59 +0100

    [c] flask-mega-tutorial/microblog/app/routes.py [c] flask-mega-tutorial/microblog/app/templates/index.html add posts loop

--- flask-mega-tutorial/microblog/app/routes.py
+++ flask-mega-tutorial/microblog/app/routes.py
@@ -5,4 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'name': 'cdbn'}
-    return render_template('index.html', title='title', user=user)
+    posts = [
+        {
+            'author': {'name': 'cdbn'},
+            'title': 'US bomb suspect warned Trump on Facebook',
+        },
+        {
+            'author': {'name': 'cdbn'},
+            'title': 'Last Jedi has critics in raptures',
+        },
+        {
+            'author': {'name': 'cdbn'},
+            'title': 'This is how you respond to a Golden Globes snub',
+        }
+    ]
+    return render_template('index.html', title='title', user=user, posts=posts)
