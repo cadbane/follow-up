0ae9d0f8982db856f4bb5950f88b1619e20a5c35
cadbane <33034420+cadbane@users.noreply.github.com>
2017-12-13 21:59:29 +0100

    add login form + view + route

--- flask-mega-tutorial/microblog/app/routes.py
+++ flask-mega-tutorial/microblog/app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,8 @@ def index():
         }
     ]
     return render_template('index.html', title='title', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', title='Log in', form=login_form)
